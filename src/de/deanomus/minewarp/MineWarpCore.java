0237f3ed4376277c3c7bf343dc76150ae0512955
Dean Reinhardt <d_reinhardt02@hotmail.com>
2020-08-30 15:36:36 +0200

    javaplugin annotations added for autogeneration of plugin.yml

--- src/de/deanomus/minewarp/MineWarpCore.java
+++ src/de/deanomus/minewarp/MineWarpCore.java
@@ -1,10 +1,30 @@
 package de.deanomus.minewarp;
 
-public class MineWarpCore {
+import de.deanomus.minewarp.util.cfg.Data;
+import org.bukkit.Bukkit;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.annotation.plugin.Description;
+import org.bukkit.plugin.java.annotation.plugin.LogPrefix;
+import org.bukkit.plugin.java.annotation.plugin.Plugin;
+import org.bukkit.plugin.java.annotation.plugin.Website;
+import org.bukkit.plugin.java.annotation.plugin.author.Author;
+
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+
+@Plugin(name = "MineWarp", version = "1.0.0")
+@Description(value = "MineWarp is a simple warpsystem for minecraft servers")
+@Author(value = "deanomus")
+@Website(value = "reinhardt.work")
+@LogPrefix(value = "MineWarp")
+public class MineWarpCore extends JavaPlugin {
 
 
     public void onEnable() {
+        ConsoleCommandSender cs = Bukkit.getConsoleSender();
 
+        getLogger().log(Level.INFO, "Starting...");
     }
 
     public void onDisable() {
